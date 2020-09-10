86d295f43c10c7c8eadac8bdd8610a8fad652ce9
Joey <joeymchau@gmail.com>
2020-09-09 20:12:05 -0400

    combine .buttons with .keyNumbers and .arrows

--- app.js
+++ app.js
@@ -16,36 +16,51 @@ const komako = {};
 // o display countdown timer (time = 5s)
 // o hide cheat code
 // o event listener for keypresses:
-//     o const empty array for user's answers
+//     x const empty array for user's answers
 //     o for each keypress, convert e.which to letter on keyboard
-//     o push value to answer array
-//     o display number of keypresses (array indices + 1); ends at 10
+//     x push value to answer array
+//     x display number of keypresses (array indices + 1); ends at 10
 // o compare cheat code array at [i] to answer array at [i]:
 //     o for each i in answer array, if !== to cheat code array; score -= 1; else score += 1;
 // o display score out of 10
 // o show button with event listener to play again (same trigger as modal button)
 
-komako.buttons = ["W", "A", "S", "D", "K", "L"];
-komako.keyNumbers = {
-    119: "W",
-    97: "A",
-    115: "S",
-    100: "D",
-    107: "K",
-    108: "L"
-}
-komako.arrows = {
-    W: `<i class="far fa-arrow-alt-circle-up"></i>`,
-    A: `<i class="far fa-arrow-alt-circle-left"></i>`,
-    S: `<i class="far fa-arrow-alt-circle-down"></i>`,
-    D: `<i class="far fa-arrow-alt-circle-right"></i>`,
-    K: `<p class="ABbutton">K</p>`,
-    L: `<p class="ABbutton">L</p>`,
+komako.buttons = [
+    {
+        letter: "W",
+        keyNumbers: 119,
+        arrows: `<i class="far fa-arrow-alt-circle-up"></i>`
+    },
+    {
+        letter: "A",
+        keyNumbers: 97,
+        arrows: `<i class="far fa-arrow-alt-circle-left"></i>`
+    },
+    {
+        letter: "S",
+        keyNumbers: 115,
+        arrows: `<i class="far fa-arrow-alt-circle-down"></i>`
+    },
+    {
+        letter: "D",
+        keyNumbers: 100,
+        arrows: `<i class="far fa-arrow-alt-circle-right"></i>`
+    },
+    {
+        letter: "K",
+        keyNumbers: 107,
+        arrows: `<p class="ABbutton">K</p>`
+    },
+    {
+        letter: "L",
+        keyNumbers: 108,
+        arrows: `<p class="ABbutton">L</p>`
+    }
+];
 
-}
-komako.convertToArrows = (array) => {
+komako.displayArrows = (array) => {
     array.forEach(el => {
-        let arrowSymb = komako.arrows[el];
+        let arrowSymb = el.arrows;
         $("#matchCode").append(`<li>${arrowSymb}</li>`);
     });
 }
@@ -53,15 +68,12 @@ komako.convertToArrows = (array) => {
 komako.random = (wasd) => rng = Math.floor(Math.random() * wasd);
 
 komako.generate = []
-// generating random code
 komako.generateCode = (size) => {
     for (let i = 0; i < size; i++) {
         let random = komako.random(komako.buttons.length);
         komako.generate.push(komako.buttons[random]);
     }
-    console.log(komako.generate.join(" "));
-    // convert WASD to arrows
-    komako.convertToArrows(komako.generate);
+    komako.displayArrows(komako.generate);
 }
 
 komako.guessCode = [];
@@ -69,7 +81,7 @@ komako.userEnterCode = (size) => {
     $(document).on("keypress", (e) => {
         if (komako.guessCode.length > size - 1) return;
         else {
-            console.log(komako.guessCode.length);
+
             komako.guessCode.push(e.which);
         }
         $("#counter").text(komako.guessCode.length);
